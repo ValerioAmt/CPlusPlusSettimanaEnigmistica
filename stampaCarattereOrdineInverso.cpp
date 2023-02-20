69dbb6270ee65d6c870adb08c44f89a162428672
valerio <valerioamato@hotmail.it>
2023-02-20 11:45:20 +0100

    commento

--- stampaCarattereOrdineInverso.cpp
+++ stampaCarattereOrdineInverso.cpp
@@ -8,7 +8,7 @@ string parolaInserita;                  //facciamo inserire una parola
 cout << "Inserisci una parola\n";
 cin >> parolaInserita;
                                          //utilizziamo un ciclo di for 
-for (int i = (parolaInserita.length());i > 0;i--)
+for (int i = (parolaInserita.length());i > 0;i--)   // il for sarà per i che va dall'ultima lettera della parola alla prima e la stamperà
                 cout << parolaInserita[i - 1] << endl ;
 
 
