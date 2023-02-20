d63741072c63fb44251677b462df002d19715ad2
valerio <valerioamato@hotmail.it>
2023-02-20 12:46:18 +0100

    prova esercizio 3

--- inputStringaContaQuanteVolteCarattere.cpp
+++ inputStringaContaQuanteVolteCarattere.cpp
@@ -9,15 +9,20 @@ int main()
     cin >> inserimento;
     int conta = 0;
     int i = 0;
+    int y;
+    int z;
     while (i < inserimento.length())
     {
-        for(y=inserimento[0];i<inserimento.length();y++)
+        for(y=inserimento[0];i<inserimento.length();y++)                     //posizione y che confronterà i valori con altra posizione z
                 
+                for(z = inserimento[+1]; z < inserimento.length(); z++){
+                       if (y = z){
+                        conta += 1;
+                       }
+                } 
 
 
-        if (inserimento[i] == inserimento[i + 1])
-        { conta + 1;
-        }
+        
         
     }
     cout << conta;
